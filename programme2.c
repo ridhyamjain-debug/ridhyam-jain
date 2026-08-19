8e688c37fe482e56d97dfdbe70a94b28d2f8b447
ridhyamjain-c <ridhyamjain@gmail.com>
2026-08-19 13:18:47 +0530

    Initial commit

---
+++ programme2.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main(){
+int a=10;
+int b=2;
+int sum=a+b;
+int diff=a-b;
+int pro=a*b;
+int div=a/b;
+int rem=a%b;
+printf("the addition of a and b is %d",sum);
+printf("the difference of a and b is %d",diff);
+printf("the product of a and b is %d",pro);
+printf("the division of a and b is%d",div);
+printf("the remainder of a and b is %d",rem);
+return 0;
+}
