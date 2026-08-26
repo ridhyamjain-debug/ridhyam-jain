bcefd796d90fdb4436a93e84567eb1b709d73e82
ridhyamjain-debug <ridhyamjain@gmail.com>
2026-08-26 12:45:56 +0530

    Add files via upload

---
+++ programme17q33.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () {
+int n, temp, digit, sum=0;
+printf ("enter value:");
+scanf("%d", &n);
+temp= n;
+while (temp>0)
+ {
+digit=temp% 10;
+sum=sum +digit *digit*digit;
+temp=temp/10;
+}
+if(sum==n)
+printf("armstrong");
+else
+printf("not armstrong");
+return 0;
+}
